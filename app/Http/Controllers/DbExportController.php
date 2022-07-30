937bcf9f56372d38ff3167460570ffd395f06efb
Liang <xl.liang2019@gmail.com>
2022-07-31 05:39:44 +0800

    fix db dump

--- app/Http/Controllers/DbExportController.php
+++ app/Http/Controllers/DbExportController.php
@@ -8,12 +8,13 @@ use Spatie\DbDumper\Databases\MySql;
 class DbExportController extends Controller
 {
     public function export() {
+        $pathToFile = 'dump.sql';
         MySql::create()
             ->setDbName(config('database.connections.mysql.database'))
             ->setUserName(config('database.connections.mysql.username'))
             ->setPassword(config('database.connections.mysql.password'))
-            ->dumpToFile('dump.sql');
+            ->dumpToFile($pathToFile);
 
-        return  Storage::download('dump.sql');
+        return  response()->download($pathToFile);
     }
 }
